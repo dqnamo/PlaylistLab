e6219205877c91a670169db97d5ae3bec76b9235
John <johnarpaul@gmail.com>
2019-09-30 19:32:10 +0100

    clean up code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,11 @@ class HomeController < ApplicationController
   require 'rspotify'
 
   def index
-
-    RSpotify.authenticate("58461ec83b874d08a8bcd731b61e09a3", "7017d0574a0c4bf4b31f5dd68f072c13")
-    me = RSpotify::User.find('alanbiju79')
-    @test = me.playlists
-
   end
 
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    @playlists = @spotify_user.playlists
   end
 
 end
