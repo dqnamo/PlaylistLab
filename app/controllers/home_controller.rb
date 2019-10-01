f83d4bfdeb807ff0a8e6fe726a8700ded1ff6006
John <johnarpaul@gmail.com>
2019-10-01 16:36:50 +0100

    added energy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,27 @@ class HomeController < ApplicationController
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     @playlists = @spotify_user.playlists
+    get_energy
+    render :playlists
   end
 
+  def playlists
+    get_energy
+  end
+
+  private
+
+  def get_energy
+    @energy = Array.new
+    total_energy = 0
+
+    @playlists.each do |x|
+      x.tracks.each do |i|
+        total_energy += i.audio_features.energy
+      end
+      energy_average = total_energy/x.tracks.count
+      @energy.push energy_average
+      total_energy = 0;
+    end
+  end
 end
