926a6710b90626f706d2acfcf058b355cef9911e
John <johnarpaul@gmail.com>
2019-10-03 15:36:56 +0100

    views

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get '/auth/spotify/callback', to: 'home#spotify'
+
+  get '/playlist/:name/:id', to: 'home#playlist', as: :playlist
 end
